62e035f0c8d9f36699c64931aef4d78f2790bea3
Rahi <iftekharhossainrahi@gmail.com>
2023-12-22 15:45:33 +0600

    Commit

---
+++ Lab Answer/Brushing-up CSE110 in after tarc vacation (22.12)/Assignment 2/Task 3.py
@@ -0,0 +1,9 @@
+first=int(input())
+second=int(input())
+if first>second:
+    print("First is greater")
+else:
+    if first==second:
+        print("The numbers are equal")
+    else:
+        print("Second is greater")
---
+++ Lab Answer/Brushing-up CSE110 in after tarc vacation (22.12)/Assignment 2/Task 4.py
@@ -0,0 +1,10 @@
+num_1=int(input())
+num_2=int(input())
+if num_1>num_2:
+    ans=num_1-num_2
+else:
+    if num_1==num_2:
+        ans=num_1-num_2
+    else:
+        ans=num_2-num_1
+print(ans)
